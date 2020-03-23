8f3b9f42697d2913e158bce66457f232f4330017
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-03-23 14:13:38 -0300

    Limpando arquivos desnecessários

--- src/derivative/Derivative.hpp
+++
@@ -1,8 +0,0 @@
-class Derivative
-{
-public:
-    Derivative();
-    double forward(double x, double deltaX, double function (double));
-    double backward(double x, double deltaX, double function (double));
-    double centered(double x, double deltaX, double function (double));
-};
