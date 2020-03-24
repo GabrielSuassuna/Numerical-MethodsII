d35dde196207bb5101b5cbd50ee3c0a8270a0ab4
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-03-24 16:26:19 -0300

    Corrigindo problemas anteriores e implementando método do Creto

--- main.cpp
+++ main.cpp
@@ -8,29 +8,29 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv){
-    Mat image;
+    Mat inputImage;
+    Mat outputImage;
     int rows;
     int columns;
     int option;
 
-    image = imread("batista.png",CV_LOAD_IMAGE_GRAYSCALE);
+    inputImage = imread("cubosCreto.png",CV_LOAD_IMAGE_GRAYSCALE);
+    outputImage = Mat::zeros(Size(inputImage.cols,inputImage.rows),CV_8UC1);
 
-    if(!image.data){
+    if(!inputImage.data){
         cout << "Tem imagem não cara" << endl;
         return -1;
     }
 
-    
-    //Cria a Janela
-    namedWindow("Janela Incrivel", cv::WINDOW_AUTOSIZE);
 
     //Função nativa no openCV para criar bordas
     //copyMakeBorder(src, output, top, bottom, left, right, Border_Type = Border_Constant, grayScale color)
-    copyMakeBorder(image, image, 1,1,1,1, BORDER_CONSTANT,0);
+    copyMakeBorder(inputImage, inputImage, 1,1,1,1, BORDER_CONSTANT,0);
     
     //Armazena número de linhas e colunas após adição da borda
-    rows = image.rows;
-    columns = image.cols;
+    rows = inputImage.rows;
+    columns = inputImage.cols;
+
 
     cout << "Para Box Blur digite 1 (Calculo com os pixel imediatamente a esquerda, direita, cima e abaixo)" << "\n";
     cout << "Para filtro gauseano digite 2 (Calculo com todos os pixels ao redor)" << "\n";
@@ -40,58 +40,64 @@ int main(int argc, char** argv){
     cin >> option;
 
     if(option == 1){
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < columns; j++){
-                image.at<uchar>(i,j) = ((int)image.at<uchar>(i-1,j) + (int)image.at<uchar>(i+1,j) + 
-                                        (int)image.at<uchar>(i,j-1) + (int)image.at<uchar>(i,j+1))/4;
+        for(int i = 1; i < rows -1; i++){
+            for(int j = 1; j < columns -1; j++){
+                outputImage.at<uchar>(i-1,j-1) = abs(((int)inputImage.at<uchar>(i-1,j) + (int)inputImage.at<uchar>(i+1,j) + 
+                                        (int)inputImage.at<uchar>(i,j-1) + (int)inputImage.at<uchar>(i,j+1))/4);
             }
         }
     }
     else if(option == 2){
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < columns; j++){
-                image.at<uchar>(i,j) = ((int)image.at<uchar>(i-1,j+1) + ((int)image.at<uchar>(i,j+1)*2) + 
-                                        (int)image.at<uchar>(i+1,j+1) + ((int)image.at<uchar>(i-1,j)*2) +
-                                        ((int)image.at<uchar>(i,j)*4)+ ((int)image.at<uchar>(i+1,j)*2) + 
-                                        (int)image.at<uchar>(i-1,j-1) + ((int)image.at<uchar>(i,j-1)*2)+
-                                        (int)image.at<uchar>(i+1,j-1)) / 16;
+        for(int i = 1; i < rows -1; i++){
+            for(int j = 1; j < columns -1; j++){
+                outputImage.at<uchar>(i-1,j-1) = ((int)inputImage.at<uchar>(i-1,j+1) + ((int)inputImage.at<uchar>(i,j+1)*2) + 
+                                        (int)inputImage.at<uchar>(i+1,j+1) + ((int)inputImage.at<uchar>(i-1,j)*2) +
+                                        ((int)inputImage.at<uchar>(i,j)*4)+ ((int)inputImage.at<uchar>(i+1,j)*2) + 
+                                        (int)inputImage.at<uchar>(i-1,j-1) + ((int)inputImage.at<uchar>(i,j-1)*2)+
+                                        (int)inputImage.at<uchar>(i+1,j-1)) / 16;
             }
         }
     }
     //Sharppen Effect
     else if(option == 3){
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < columns; j++){
-                image.at<uchar>(i,j) = (5*(int)image.at<uchar>(i,j) - (int)image.at<uchar>(i-1,j) - 
-                                        (int)image.at<uchar>(i+1,j) - (int)image.at<uchar>(i,j-1) -
-                                        (int)image.at<uchar>(i,j+1));
+        for(int i = 1; i < rows-1; i++){
+            for(int j = 1; j < columns-1; j++){
+                outputImage.at<uchar>(i-1,j-1) = (5*(int)inputImage.at<uchar>(i,j) - (int)inputImage.at<uchar>(i-1,j) - 
+                                        (int)inputImage.at<uchar>(i+1,j) - (int)inputImage.at<uchar>(i,j-1) -
+                                        (int)inputImage.at<uchar>(i,j+1));
             }
         }
     }
     //Edge Detection
     else if(option == 4){
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < columns; j++){
-                image.at<uchar>(i,j) = ((int)image.at<uchar>(i-1,j) - (int)image.at<uchar>(i,j-1) +
-                                        (int)image.at<uchar>(i,j+1) -(int)image.at<uchar>(i+1,j));
+        for(int i = 1; i < rows-1; i++){
+            for(int j = 1; j < columns-1; j++){
+                outputImage.at<uchar>(i-1,j-1) = ((int)inputImage.at<uchar>(i-1,j) - (int)inputImage.at<uchar>(i,j-1) +
+                                        (int)inputImage.at<uchar>(i,j+1) -(int)inputImage.at<uchar>(i+1,j));
             }
         }
     }
-    else if(option ==5){
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < columns; j++){
-                image.at<uchar>(i,j) = -2*( (1/(image.at<uchar>(i+1,j) - 2*image.at<uchar>(i,j) + image.at<uchar>(i-1,j))) +
-                                            (1/(image.at<uchar>(i,j+1) - 2*image.at<uchar>(i,j) + image.at<uchar>(i,j-1))));
+    else if(option == 5){
+        for(int i = 1; i < rows - 1; i++){
+            for(int j = 1; j < columns - 1; j++){
+                outputImage.at<uchar>(i-1,j-1) = (abs((static_cast<int>(inputImage.at<uchar>(i+1,j))/2) - (static_cast<int>(inputImage.at<uchar>(i-1,j))/2) +
+                                                abs((static_cast<int>(inputImage.at<uchar>(i,j+1))/2) - (static_cast<int>(inputImage.at<uchar>(i,j-1))/2))));
             }
         }
     }
     else{
-        cout << "Opção inexistente, tente de novo!";
+        cout << "Opção inexistente, tente de novo!" << "\n";
     }
-    
 
 
-    imshow("Janela Incrivel", image);
+
+    //Cria a Janela
+    namedWindow("Janela Incrivel", WINDOW_AUTOSIZE);
+    imshow("Janela Incrivel", inputImage);
+
+
+    namedWindow("Janela Incrivel(alterada)", WINDOW_AUTOSIZE);
+    imshow("Janela Incrivel (alterada)", outputImage);
 
     waitKey();
 
